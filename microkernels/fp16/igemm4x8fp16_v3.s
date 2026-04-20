426f317a595d27f19d836dc1b8ef2ac9384c53ca
mohammad-ghaderi <mohammadunique2002@gmail.com>
2026-04-20 23:09:44 +0330

    i don't use fp16 version, conversion takes too much time, arm64v8 does not have needed fp16 instructions, just keep these files here

---
+++ microkernels/fp16/igemm4x8fp16_v3.s
@@ -0,0 +1,183 @@
+.global igemm4x8fp16_v3
+.type igemm4x8fp16_v3, %function
+
+
+
+igemm4x8fp16_v3:
+    // x0: indirction input address
+    // x1: weights address
+    // x2: output address
+    // x3: output stride
+    // x4: IC
+
+    movi    v16.4s, #0
+    movi    v17.4s, #0
+
+    movi    v18.4s, #0
+    movi    v19.4s, #0
+
+    movi    v20.4s, #0
+    movi    v21.4s, #0
+    
+    movi    v22.4s, #0
+    movi    v23.4s, #0
+
+    mov     x15, #9             // k idx
+kernel_loop:
+
+    ldr     x10, [x0]            // pixel0
+    ldr     x11, [x0, 72]        // pixel1 (+ 9*8)
+    ldr     x12, [x0, 144]       // pixel2 (+ 18*8)
+    ldr     x13, [x0, 216]       // pixel3 (+ 27*8)
+
+    mov     x16, x4             // ic idx
+ic_loop:
+    // load inputs
+
+    ld1         {v0.8h}, [x10], 16
+    ld1         {v1.8h}, [x11], 16
+    ld1         {v2.8h}, [x12], 16
+    ld1         {v3.8h}, [x13], 16
+    
+    // convert float16 -> float32
+    fcvtl2      v24.4s, v0.8h
+    fcvtl2      v25.4s, v1.8h
+    fcvtl2      v26.4s, v2.8h
+    fcvtl2      v27.4s, v3.8h
+    fcvtl       v0.4s, v0.4h
+    fcvtl       v1.4s, v1.4h
+    fcvtl       v2.4s, v2.4h
+    fcvtl       v3.4s, v3.4h
+
+    //                                      input: v0, v1, v2, v3
+    // load weights                         weights: v4, v5
+
+    ld1         {v6.8h, v7.8h}, [x1], 32
+    fcvtl2      v5.4s, v6.8h
+    fcvtl       v4.4s, v6.4h
+
+    fmla        v16.4s, v4.4s, v0.s[0]
+    fmla        v17.4s, v5.4s, v0.s[0]
+    fmla        v18.4s, v4.4s, v1.s[0]
+    fmla        v19.4s, v5.4s, v1.s[0]
+    fmla        v20.4s, v4.4s, v2.s[0]
+    fmla        v21.4s, v5.4s, v2.s[0]
+    fmla        v22.4s, v4.4s, v3.s[0]
+    fmla        v23.4s, v5.4s, v3.s[0]
+
+    fcvtl2      v5.4s, v7.8h
+    fcvtl       v4.4s, v7.4h
+
+    fmla        v16.4s, v4.4s, v0.s[1]
+    fmla        v17.4s, v5.4s, v0.s[1]
+    fmla        v18.4s, v4.4s, v1.s[1]
+    fmla        v19.4s, v5.4s, v1.s[1]
+    fmla        v20.4s, v4.4s, v2.s[1]
+    fmla        v21.4s, v5.4s, v2.s[1]
+    fmla        v22.4s, v4.4s, v3.s[1]
+    fmla        v23.4s, v5.4s, v3.s[1]
+
+    ld1         {v6.8h, v7.8h}, [x1], 32
+    fcvtl2      v5.4s, v6.8h
+    fcvtl       v4.4s, v6.4h
+
+    fmla        v16.4s, v4.4s, v0.s[2]
+    fmla        v17.4s, v5.4s, v0.s[2]
+    fmla        v18.4s, v4.4s, v1.s[2]
+    fmla        v19.4s, v5.4s, v1.s[2]
+    fmla        v20.4s, v4.4s, v2.s[2]
+    fmla        v21.4s, v5.4s, v2.s[2]
+    fmla        v22.4s, v4.4s, v3.s[2]
+    fmla        v23.4s, v5.4s, v3.s[2]
+
+    fcvtl2      v5.4s, v7.8h
+    fcvtl       v4.4s, v7.4h
+
+    fmla        v16.4s, v4.4s, v0.s[3]
+    fmla        v17.4s, v5.4s, v0.s[3]
+    fmla        v18.4s, v4.4s, v1.s[3]
+    fmla        v19.4s, v5.4s, v1.s[3]
+    fmla        v20.4s, v4.4s, v2.s[3]
+    fmla        v21.4s, v5.4s, v2.s[3]
+    fmla        v22.4s, v4.4s, v3.s[3]
+    fmla        v23.4s, v5.4s, v3.s[3]
+
+    ld1         {v6.8h, v7.8h}, [x1], 32
+    fcvtl2      v5.4s, v6.8h
+    fcvtl       v4.4s, v6.4h
+
+    fmla        v16.4s, v4.4s, v24.s[0]
+    fmla        v17.4s, v5.4s, v24.s[0]
+    fmla        v18.4s, v4.4s, v25.s[0]
+    fmla        v19.4s, v5.4s, v25.s[0]
+    fmla        v20.4s, v4.4s, v26.s[0]
+    fmla        v21.4s, v5.4s, v26.s[0]
+    fmla        v22.4s, v4.4s, v27.s[0]
+    fmla        v23.4s, v5.4s, v27.s[0]
+
+    fcvtl2      v5.4s, v7.8h
+    fcvtl       v4.4s, v7.4h
+
+    fmla        v16.4s, v4.4s, v24.s[1]
+    fmla        v17.4s, v5.4s, v24.s[1]
+    fmla        v18.4s, v4.4s, v25.s[1]
+    fmla        v19.4s, v5.4s, v25.s[1]
+    fmla        v20.4s, v4.4s, v26.s[1]
+    fmla        v21.4s, v5.4s, v26.s[1]
+    fmla        v22.4s, v4.4s, v27.s[1]
+    fmla        v23.4s, v5.4s, v27.s[1]
+
+    ld1         {v6.8h, v7.8h}, [x1], 32
+    fcvtl2      v5.4s, v6.8h
+    fcvtl       v4.4s, v6.4h
+
+    fmla        v16.4s, v4.4s, v24.s[2]
+    fmla        v17.4s, v5.4s, v24.s[2]
+    fmla        v18.4s, v4.4s, v25.s[2]
+    fmla        v19.4s, v5.4s, v25.s[2]
+    fmla        v20.4s, v4.4s, v26.s[2]
+    fmla        v21.4s, v5.4s, v26.s[2]
+    fmla        v22.4s, v4.4s, v27.s[2]
+    fmla        v23.4s, v5.4s, v27.s[2]
+
+    fcvtl2      v5.4s, v7.8h
+    fcvtl       v4.4s, v7.4h
+
+    fmla        v16.4s, v4.4s, v24.s[3]
+    fmla        v17.4s, v5.4s, v24.s[3]
+    fmla        v18.4s, v4.4s, v25.s[3]
+    fmla        v19.4s, v5.4s, v25.s[3]
+    fmla        v20.4s, v4.4s, v26.s[3]
+    fmla        v21.4s, v5.4s, v26.s[3]
+    fmla        v22.4s, v4.4s, v27.s[3]
+    fmla        v23.4s, v5.4s, v27.s[3]
+
+
+    subs x16, x16, #8
+    bgt ic_loop
+
+    adds x0, x0, #8
+    subs x15, x15, #1
+    bgt kernel_loop
+
+    // store outputs
+
+    st1 {v16.4s}, [x2], #16
+    st1 {v17.4s}, [x2], #16
+
+    add x2, x2, x3
+
+    st1 {v18.4s}, [x2], #16
+    st1 {v19.4s}, [x2], #16
+
+    add x2, x2, x3
+
+    st1 {v20.4s}, [x2], #16
+    st1 {v21.4s}, [x2], #16
+
+    add x2, x2, x3
+
+    st1 {v22.4s}, [x2], #16
+    st1 {v23.4s}, [x2], #16
+
+    ret
