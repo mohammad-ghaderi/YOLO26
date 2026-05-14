c62281dbde2f1774721b165f1f68e68345e90a08
mohammad-ghaderi <mohammadunique2002@gmail.com>
2026-05-14 18:59:08 +0330

    two type for implementation for pointwise conv1x1

---
+++ microkernels/pointwise5x20.s
@@ -0,0 +1,283 @@
+.global point_wise_5x20
+.type point_wise_5x20, %function
+
+point_wise_5x20:
+    // x0: input address
+    // x1: weights address
+    // x2: output address
+    // x3: IC
+    // x4: OC
+    // x5: output stride
+
+    sub     sp, sp, #128
+    stp     q8, q9, [sp, #0]
+    stp     q10, q11, [sp, #32]
+    stp     q12, q13, [sp, #64]
+    stp     q14, q15, [sp, #96]
+
+    movi    v0.4s, #0
+    movi    v1.4s, #0
+    movi    v2.4s, #0
+    movi    v3.4s, #0
+    movi    v4.4s, #0
+
+    movi    v12.4s, #0
+    movi    v13.4s, #0
+    movi    v14.4s, #0
+    movi    v15.4s, #0
+    movi    v16.4s, #0
+
+    movi    v17.4s, #0
+    movi    v18.4s, #0
+    movi    v19.4s, #0
+    movi    v20.4s, #0
+    movi    v21.4s, #0
+
+    movi    v22.4s, #0
+    movi    v23.4s, #0
+    movi    v24.4s, #0
+    movi    v25.4s, #0
+    movi    v26.4s, #0
+
+    movi    v27.4s, #0
+    movi    v28.4s, #0
+    movi    v29.4s, #0
+    movi    v30.4s, #0
+    movi    v31.4s, #0
+
+    lsl     x10, x3, #2
+
+    mov     x11, x0             // inp 1
+    add     x12, x11, x10       // inp 2
+    add     x13, x12, x10       // inp 3
+    add     x14, x13, x10       // inp 4
+    add     x15, x14, x10       // inp 5
+
+
+    mov x9, x3
+.ic_loop:
+
+    // v0,  v1,  v2,  v3,  v4
+    // v12, v13, v14, v15, v16
+    // v17, v18, v19, v20, v21
+    // v22, v23, v24, v26, v26
+    // v27, v28, v29, v30, v31`
+
+    ld1     {v5.4s}, [x11], #16
+    ld1     {v6.4s}, [x12], #16
+    ld1     {v7.4s}, [x13], #16
+    ld1     {v8.4s}, [x14], #16
+    ld1     {v9.4s}, [x15], #16
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[0,3]
+
+    fmla    v0.4s,  V10.4S, v5.S[0]
+    fmla    v12.4s, V10.4S, v6.S[0]
+    fmla    v17.4s, V10.4S, v7.S[0]
+    fmla    v22.4s, V10.4S, v8.S[0]
+    fmla    v27.4s, V10.4S, v9.S[0]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[4,7]
+
+    fmla    v1.4s,  V10.4S, v5.S[0]
+    fmla    v13.4s, V10.4S, v6.S[0]
+    fmla    v18.4s, V10.4S, v7.S[0]
+    fmla    v23.4s, V10.4S, v8.S[0]
+    fmla    v28.4s, V10.4S, v9.S[0]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[8,11]
+
+    fmla    v2.4s,  V10.4S, v5.S[0]
+    fmla    v14.4s, V10.4S, v6.S[0]
+    fmla    v19.4s, V10.4S, v7.S[0]
+    fmla    v24.4s, V10.4S, v8.S[0]
+    fmla    v29.4s, V10.4S, v9.S[0]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[12, 15]
+
+    fmla    v3.4s,  V10.4S, v5.S[0]
+    fmla    v15.4s, V10.4S, v6.S[0]
+    fmla    v20.4s, V10.4S, v7.S[0]
+    fmla    v25.4s, V10.4S, v8.S[0]
+    fmla    v30.4s, V10.4S, v9.S[0]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[16, 19]
+
+    fmla    v4.4s,  V10.4S, v5.S[0]
+    fmla    v16.4s, V10.4S, v6.S[0]
+    fmla    v21.4s, V10.4S, v7.S[0]
+    fmla    v26.4s, V10.4S, v8.S[0]
+    fmla    v31.4s, V10.4S, v9.S[0]
+
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[0,3]
+
+    fmla    v0.4s,  V10.4S, v5.S[1]
+    fmla    v12.4s, V10.4S, v6.S[1]
+    fmla    v17.4s, V10.4S, v7.S[1]
+    fmla    v22.4s, V10.4S, v8.S[1]
+    fmla    v27.4s, V10.4S, v9.S[1]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[4,7]
+
+    fmla    v1.4s,  V10.4S, v5.S[1]
+    fmla    v13.4s, V10.4S, v6.S[1]
+    fmla    v18.4s, V10.4S, v7.S[1]
+    fmla    v23.4s, V10.4S, v8.S[1]
+    fmla    v28.4s, V10.4S, v9.S[1]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[8,11]
+
+    fmla    v2.4s,  V10.4S, v5.S[1]
+    fmla    v14.4s, V10.4S, v6.S[1]
+    fmla    v19.4s, V10.4S, v7.S[1]
+    fmla    v24.4s, V10.4S, v8.S[1]
+    fmla    v29.4s, V10.4S, v9.S[1]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[12, 15]
+
+    fmla    v3.4s,  V10.4S, v5.S[1]
+    fmla    v15.4s, V10.4S, v6.S[1]
+    fmla    v20.4s, V10.4S, v7.S[1]
+    fmla    v25.4s, V10.4S, v8.S[1]
+    fmla    v30.4s, V10.4S, v9.S[1]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[16, 19]
+
+    fmla    v4.4s,  V10.4S, v5.S[1]
+    fmla    v16.4s, V10.4S, v6.S[1]
+    fmla    v21.4s, V10.4S, v7.S[1]
+    fmla    v26.4s, V10.4S, v8.S[1]
+    fmla    v31.4s, V10.4S, v9.S[1]
+
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[0,3]
+
+    fmla    v0.4s,  V10.4S, v5.S[2]
+    fmla    v12.4s, V10.4S, v6.S[2]
+    fmla    v17.4s, V10.4S, v7.S[2]
+    fmla    v22.4s, V10.4S, v8.S[2]
+    fmla    v27.4s, V10.4S, v9.S[2]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[4,7]
+
+    fmla    v1.4s,  V10.4S, v5.S[2]
+    fmla    v13.4s, V10.4S, v6.S[2]
+    fmla    v18.4s, V10.4S, v7.S[2]
+    fmla    v23.4s, V10.4S, v8.S[2]
+    fmla    v28.4s, V10.4S, v9.S[2]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[8,11]
+
+    fmla    v2.4s,  V10.4S, v5.S[2]
+    fmla    v14.4s, V10.4S, v6.S[2]
+    fmla    v19.4s, V10.4S, v7.S[2]
+    fmla    v24.4s, V10.4S, v8.S[2]
+    fmla    v29.4s, V10.4S, v9.S[2]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[12, 15]
+
+    fmla    v3.4s,  V10.4S, v5.S[2]
+    fmla    v15.4s, V10.4S, v6.S[2]
+    fmla    v20.4s, V10.4S, v7.S[2]
+    fmla    v25.4s, V10.4S, v8.S[2]
+    fmla    v30.4s, V10.4S, v9.S[2]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[16, 19]
+
+    fmla    v4.4s,  V10.4S, v5.S[2]
+    fmla    v16.4s, V10.4S, v6.S[2]
+    fmla    v21.4s, V10.4S, v7.S[2]
+    fmla    v26.4s, V10.4S, v8.S[2]
+    fmla    v31.4s, V10.4S, v9.S[2]
+
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[0,3]
+
+    fmla    v0.4s,  V10.4S, v5.S[3]
+    fmla    v12.4s, V10.4S, v6.S[3]
+    fmla    v17.4s, V10.4S, v7.S[3]
+    fmla    v22.4s, V10.4S, v8.S[3]
+    fmla    v27.4s, V10.4S, v9.S[3]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[4,7]
+
+    fmla    v1.4s,  V10.4S, v5.S[3]
+    fmla    v13.4s, V10.4S, v6.S[3]
+    fmla    v18.4s, V10.4S, v7.S[3]
+    fmla    v23.4s, V10.4S, v8.S[3]
+    fmla    v28.4s, V10.4S, v9.S[3]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[8,11]
+
+    fmla    v2.4s,  V10.4S, v5.S[3]
+    fmla    v14.4s, V10.4S, v6.S[3]
+    fmla    v19.4s, V10.4S, v7.S[3]
+    fmla    v24.4s, V10.4S, v8.S[3]
+    fmla    v29.4s, V10.4S, v9.S[3]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[12, 15]
+
+    fmla    v3.4s,  V10.4S, v5.S[3]
+    fmla    v15.4s, V10.4S, v6.S[3]
+    fmla    v20.4s, V10.4S, v7.S[3]
+    fmla    v25.4s, V10.4S, v8.S[3]
+    fmla    v30.4s, V10.4S, v9.S[3]
+
+    ld1     {v10.4s}, [x1], #16     // weights oc[16, 19]
+
+    fmla    v4.4s,  V10.4S, v5.S[3]
+    fmla    v16.4s, V10.4S, v6.S[3]
+    fmla    v21.4s, V10.4S, v7.S[3]
+    fmla    v26.4s, V10.4S, v8.S[3]
+    fmla    v31.4s, V10.4S, v9.S[3]
+
+    subs x9, x9, #4
+    bgt .ic_loop
+
+    st1     {v0.4s}, [x2], #16
+    st1     {v1.4s}, [x2], #16
+    st1     {v2.4s}, [x2], #16
+    st1     {v3.4s}, [x2], #16
+    st1     {v4.4s}, [x2], #16
+
+    add     x2, x2, x5
+
+    st1     {v12.4s}, [x2], #16
+    st1     {v13.4s}, [x2], #16
+    st1     {v14.4s}, [x2], #16
+    st1     {v15.4s}, [x2], #16
+    st1     {v16.4s}, [x2], #16
+
+    add     x2, x2, x5
+
+    st1     {v17.4s}, [x2], #16
+    st1     {v18.4s}, [x2], #16
+    st1     {v19.4s}, [x2], #16
+    st1     {v20.4s}, [x2], #16
+    st1     {v21.4s}, [x2], #16
+
+    add     x2, x2, x5
+    
+    st1     {v22.4s}, [x2], #16
+    st1     {v23.4s}, [x2], #16
+    st1     {v24.4s}, [x2], #16
+    st1     {v25.4s}, [x2], #16
+    st1     {v26.4s}, [x2], #16
+
+    add     x2, x2, x5
+    
+    st1     {v27.4s}, [x2], #16
+    st1     {v28.4s}, [x2], #16
+    st1     {v29.4s}, [x2], #16
+    st1     {v30.4s}, [x2], #16
+    st1     {v31.4s}, [x2]
+
+
+    ldp     q14, q15, [sp, #96]
+    ldp     q12, q13, [sp, #64]
+    ldp     q10, q11, [sp, #32]
+    ldp     q8, q9, [sp, #0]
+    add     sp, sp, #128
+
+    ret
