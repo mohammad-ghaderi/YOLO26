3c907e4ba77a615cd9682581e4aea09348c6c230
mohammad-ghaderi <mohammadunique2002@gmail.com>
2026-05-26 17:37:28 +0330

    implemented YOLO26n for the first 3 layer

--- main.c
+++ main.c
@@ -10,11 +10,9 @@
 #include "tools/stb_image.h"
 
 
-float input[MAX_SIZE];
-float new_inp[MAX_SIZE];
-float weights[MAX_SIZE];
-float pw[MAX_SIZE];
-float output[MAX_SIZE];
+float arr1[MAX_SIZE];
+float arr2[MAX_SIZE];
+float arr3[MAX_SIZE];
 
 int SIZE = 640, IC = 3, OC = 16;
 int stride = 2;
@@ -22,62 +20,85 @@ int stride = 2;
 void SiLU(float *inp);
 void SiLU_array(float *inp, int SIZE);
 void SiLU_array_bias(float *inp, float *bias, int SIZE, int OC);
+void SiLU_array_bias_full(float *inp, float *bias, int SIZE, int OC);
+
 
 void here() {
     return;
 }
 
-void gemm_ic3s2(float *inp, float *weights, float *output, int SIZE);
+void gemm_ic3s2(float *inp, float *weights, float *arr2, int SIZE);
 
 int main() {
-    int width, height, channels;
-    unsigned char *img = stbi_load("test/img.jpg", &width, &height, &channels, 3);
-
-    for (int i = 0; i < SIZE*SIZE*IC; i++) input[IC*SIZE+i] = (float)img[i] / 255.0f;
-    
+    // int width, height, channels;
+    // unsigned char *img = stbi_load("test/img.jpg", &width, &height, &channels, 3);
+    if (load_image() != 0) {
+        printf("Failed to load image!\n");
+        return 1;
+    }
     if (load_weights() != 0) {
         printf("Failed to load weights!\n");
         return 1;
     }
 
-    int OUT = (SIZE + 2 - 3)/stride + 1;
-    // writeArrayToFile(input+IC*SIZE, SIZE*SIZE*IC, "out/input.txt", 0);
-    // writeArrayToFile(params, 9*IC*OC, "out/weights.txt", 0);
-    // writeArrayToFile(weights, 9*OC, "out/weights.txt", 0);  // for depth wise
+    float *weights = params;
     
-    memset(output, 0, OUT*OUT*OC*4);
-    // add_padding_backward(input, SIZE, IC);
-    // winograd(input, weights, output, SIZE, IC, OC);
+    for (int i = 0; i < SIZE*SIZE*IC; i++) arr1[IC*SIZE+i] = (float)img[i] / 255.0f;
+    
+    int OUT = (SIZE + 2 - 3)/stride + 1;
 
+    gemm_ic3s2(arr1, weights, arr2, SIZE);
+    weights += IC*OC*9;
+    SiLU_array_bias(arr2, weights, OUT*OUT*OC, OC);
+    weights += OC;
+
+    IC = OC; SIZE = OUT; OC = 32; OUT /= 2;
+
+    conv3x3nr8(arr2, weights, arr1, SIZE, IC, OC, stride);
+    weights += IC*OC*9;
+    SiLU_array_bias_full(arr1, weights, OUT*OUT*OC, OC);
+    weights += OC;
+    
+    IC = OC; SIZE = OUT;
+    pointwise_conv5x16(arr1, weights, arr2, IC, OC, SIZE);
+    weights += IC*OC;
+    SiLU_array_bias_full(arr2, weights, OUT*OUT*OC, OC);
+    weights += OC;
+
+    IC = 16; OC = 8;
+    int idx = 0;
+
+    for (int i = 0; i < (SIZE+2)*IC; i++) arr3[idx++] = 0;
+    for (int ih = 0; ih < SIZE; ih++) {
+        for (int ic = 0; ic < IC; ic++) arr3[idx++] = 0;
+        for (int iw = 0; iw < SIZE; iw++) {
+            for (int ic = 0; ic < IC; ic++) {
+                arr3[idx++] = arr2[(ih*SIZE+iw)*IC*2+ic+IC];
+            }
+        }
+        for (int ic = 0; ic < IC; ic++) arr3[idx++] = 0;
+    }
+    for (int i = 0; i < (SIZE+2)*IC; i++) arr3[idx++] = 0;
+
+    writeArrayToFile(weights, IC*OC*9, "out/weights.txt", 0);
+
+    winograd_f23(arr3, weights, arr1, SIZE, IC, OC);
+    weights += IC*OC;
+    SiLU_array_bias_full(arr1, weights, OUT*OUT*OC, OC);
+    
+    writeArrayToFile(arr1, OUT*OUT*OC, "out/output.txt", 0);
     here();
+    
 
-    // conv3x3(input, weights, output, SIZE, IC, OC, version, thread, block_y, block_x, stride);
-    // winograd_f23(input, weights, output, SIZE, IC, OC);
-    // pointwise_conv_simple(input, weights, output, IC, OC, SIZE);
-    // pointwise_conv5x16(input, weights, output, IC, OC, SIZE);
-    // pointwise_conv5x20(input, weights, output, IC, OC, SIZE);
-    // depthwise_conv_simple(input, weights, output, IC, OC, SIZE);
-    // depthwise_conv_c4(input, weights, output, OC, SIZE);
-    // depthwise_conv_c4r2(input, weights, output, OC, SIZE);
-    // conv3x3(input, weights, output, SIZE, IC, OC, 7, 1, 4, 4, stride);
-
-    // pack_weights_ic3(params, pw);
-    // writeArrayToFile(pw, IC*9*OC, "out/pw.txt", 0);
-    writeArrayToFile(params, IC*9*OC, "out/pw2.txt", 0);
+    // float bias[OC];
+    // add_padding_backward(arr2, SIZE, IC);
+    // winograd_f23(arr2, weights, arr1, SIZE, IC, OC);
+    
+    
+    // I should do the winograd weight transform and packing in the build time
+    
 
 
-    // clock_t start = clock();
-
-    gemm_ic3s2(input, params, output, SIZE);
-    SiLU_array_bias(output, params + IC*OC*9, OUT*OUT*OC, OC);
-    // for (int i = 0; i < OUT*OUT*OC; i+=4) SiLU(output+i);
-
-
-    // clock_t end = clock();
-    // double time_spent = (double)(end - start) / CLOCKS_PER_SEC;
-    // printf("%.6f #\n", time_spent);
-
-    writeArrayToFile(output, OUT*OUT*OC, "out/out.txt", 0);
 
 
 
