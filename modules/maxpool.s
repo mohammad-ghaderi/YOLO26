c9c9be070784387d2ec4b55c80c6fb7a86df1cb5
mohammad-ghaderi <mohammadunique2002@gmail.com>
2026-06-04 18:34:27 +0330

    inplace 3x maxpool 5x5p2

--- modules/maxpool.s
+++ modules/maxpool.s
@@ -425,4 +425,154 @@ maxpool_3_5x5:
     bgt .h_main_loop
 
 
+    /// last two rows ///
+
+    mov x5, x4
+
+    mov x1, #128
+.oc_last_loop:
+
+    add x10, x5, x2
+    add x10, x10, x2    // x10: (0, 2)
+    add x11, x10, x3    // x11: (1, 2)
+    add x12, x11, x3    // x12: (2, 2)
+    add x13, x12, x3    // x13: (3, 2)
+
+    add x6, x5, x3
+    add x6, x6, x3
+    add x6, x6, #512       // out row 0
+    add x7, x6, x3          // out row 1  
+
+    // v26, v27, v28, v29, v30, v31: are aux for loading
+
+    ld1     {v2.4s}, [x10]
+    ld1     {v18.4s}, [x11]
+    ld1     {v26.4s}, [x12]
+    fmax    v18.4s, v18.4s, v26.4s
+    ld1     {v27.4s}, [x13]
+    fmax    v18.4s, v18.4s, v27.4s
+
+    subs x10, x10, x2    // prev column
+    subs x11, x11, x2    // prev column
+    subs x12, x12, x2    // prev column
+    subs x13, x13, x2    // prev column
+
+    ld1     {v1.4s}, [x10]
+    fmax    v1.4s, v1.4s, v2.4s
+    ld1     {v17.4s}, [x11]
+    ld1     {v26.4s}, [x12]
+    fmax    v17.4s, v17.4s, v26.4s
+    ld1     {v27.4s}, [x13]
+    fmax    v17.4s, v17.4s, v27.4s
+    fmax    v17.4s, v17.4s, v18.4s
+
+    subs x10, x10, x2    // prev column
+    subs x11, x11, x2    // prev column
+    subs x12, x12, x2    // prev column
+    subs x13, x13, x2    // prev column
+
+    ld1     {v0.4s}, [x10]
+    fmax    v0.4s, v0.4s, v1.4s
+    ld1     {v16.4s}, [x11]
+    ld1     {v26.4s}, [x12]
+    fmax    v16.4s, v16.4s, v26.4s
+    ld1     {v27.4s}, [x13]
+    fmax    v16.4s, v16.4s, v27.4s
+    fmax    v16.4s, v16.4s, v17.4s
+
+    fmax    v0.4s, v0.4s, v16.4s
+
+    st1     {v0.4s}, [x6]           // out(0,0)
+    st1     {v16.4s}, [x7]          // out(1,0)
+
+    add x6, x6, x2
+    add x7, x7, x2
+
+    add x10, x5, x2
+    add x10, x10, x2
+    add x10, x10, x2
+    add x11, x10, x3
+    add x12, x11, x3
+    add x13, x12, x3
+    
+    ld1     {v3.4s}, [x10]
+    ld1     {v19.4s}, [x11]
+    ld1     {v26.4s}, [x12]
+    fmax    v19.4s, v19.4s, v26.4s
+    ld1     {v27.4s}, [x13]
+    fmax    v19.4s, v19.4s, v27.4s
+    fmax    v2.4s, v2.4s, v3.4s
+    fmax    v1.4s, v1.4s, v3.4s
+    fmax    v0.4s, v0.4s, v3.4s
+    fmax    v18.4s, v18.4s, v19.4s
+    fmax    v17.4s, v17.4s, v19.4s
+    fmax    v16.4s, v16.4s, v19.4s
+
+    fmax    v0.4s, v0.4s, v16.4s
+
+    st1     {v0.4s}, [x6]           // out(0,1)
+    st1     {v16.4s}, [x7]          // out(1,1)
+
+    mov x15, #16
+.w_last_loop:
+    add x6, x6, x2
+    add x7, x7, x2
+
+    add x10, x10, x2
+    add x11, x11, x2
+    add x12, x12, x2
+    add x13, x13, x2
+
+    ld1     {v4.4s}, [x10]
+    ld1     {v20.4s}, [x11]
+    ld1     {v26.4s}, [x12]
+    fmax    v20.4s, v20.4s, v26.4s
+    ld1     {v27.4s}, [x13]
+    fmax    v20.4s, v20.4s, v27.4s
+    fmax    v3.4s, v3.4s, v4.4s
+    fmax    v2.4s, v2.4s, v4.4s
+    fmax    v1.4s, v1.4s, v4.4s
+    fmax    v0.4s, v0.4s, v4.4s
+    fmax    v19.4s, v19.4s, v20.4s
+    fmax    v18.4s, v18.4s, v20.4s
+    fmax    v17.4s, v17.4s, v20.4s
+    fmax    v16.4s, v16.4s, v20.4s
+
+    fmax    v0.4s, v0.4s, v16.4s
+
+    st1     {v0.4s}, [x6]           // out(0,2)
+    st1     {v16.4s}, [x7]          // out(1,2)
+
+    mov     v0.16b, v1.16b
+    mov     v16.16b, v17.16b
+    mov     v1.16b, v2.16b
+    mov     v17.16b, v18.16b
+    mov     v2.16b, v3.16b
+    mov     v18.16b, v19.16b
+    mov     v3.16b, v4.16b
+    mov     v19.16b, v20.16b
+
+    subs x15, x15, #1
+    bgt .w_last_loop
+
+    add x6, x6, x2
+    add x7, x7, x2
+
+    fmax    v0.4s, v0.4s, v16.4s
+
+    st1     {v0.4s}, [x6] 
+    st1     {v16.4s}, [x7]
+
+    add x6, x6, x2
+    add x7, x7, x2
+
+    fmax    v1.4s, v1.4s, v17.4s
+
+    st1     {v1.4s}, [x6] 
+    st1     {v17.4s}, [x7]
+
+    add x5, x5, #16
+    subs x1, x1, #4
+    bgt .oc_last_loop
+
     ret
